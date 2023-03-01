5a6730543fe73675adfcf7ee7a24f3f87d2832bc
Nichkov Roman <nichkovroman@gmail.com>
2023-03-01 15:24:07 +0300

    feat: add burger menu

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,1 +1,9 @@
+export const handleBurger = () => {
+    const header = document.querySelector('.header')
+    const burgerBtn = header.querySelector('.burger')
 
+    burgerBtn.addEventListener('click', () => {
+        burgerBtn.classList.toggle('active')
+        header.classList.toggle('active')
+    })
+}
