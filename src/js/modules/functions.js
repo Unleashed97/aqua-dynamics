f865000f5fc70d63abd0259e90ace71741b4bd2c
Nichkov Roman <nichkovroman@gmail.com>
2023-03-01 23:27:23 +0300

    feat: add section faq

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -7,3 +7,27 @@ export const handleBurger = () => {
         header.classList.toggle('active')
     })
 }
+
+export const handleFaq = () => {
+    const questions = document.querySelectorAll('.faq')
+    questions.forEach((question) => {
+        const opener = question.querySelector('.faq-title')
+        const status = question.querySelector('.faq-status')
+        opener.addEventListener('click', () => {
+            ;[...questions]
+                .filter((q) => q !== question)
+                .forEach((q) => {
+                    q.classList.remove('opened')
+                    status.src = 'img/icons/plus-circle.svg'
+                })
+            question.classList.toggle('opened')
+            if (question.classList.contains('opened')) {
+                status.src = 'img/icons/minus-circle.svg'
+            } else {
+                status.src = 'img/icons/plus-circle.svg'
+            }
+
+            console.log(status)
+        })
+    })
+}
